a07a00fdaa8e07a26668b0effcb8068b0bcc47c3
Biel Frontera <biel.fb@gmail.com>
2020-04-09 11:35:40 +0200

    Fix flake8 errors in covid19plot

--- covid19plot.py
+++ covid19plot.py
@@ -3,7 +3,6 @@
 
 import pandas as pd
 import numpy as np
-from datetime import date
 import os
 from gettext import gettext as _
 import gettext
@@ -63,19 +62,19 @@ class COVID19Plot(object):
 
         self._df = pd.read_csv(csv_path)
         # convert date to datetime and set index
-        self._df['fecha']= pd.to_datetime(self._df['fecha'])
+        self._df['fecha'] = pd.to_datetime(self._df['fecha'])
         self._df.set_index(['fecha', 'cod_ine'], inplace=True)
         self._df.rename(columns={'total': 'cases'}, inplace=True)
         # column recovered
         altas_df = pd.read_csv(self._source_path + '/ccaa_covid19_altas_long.csv')
-        altas_df['fecha']= pd.to_datetime(altas_df['fecha'])
+        altas_df['fecha'] = pd.to_datetime(altas_df['fecha'])
         altas_df.set_index(['fecha', 'cod_ine'], inplace=True)
         altas_df.columns = ['CCAA_altas', 'recovered']
         self._df = self._df.merge(altas_df, left_index=True, right_index=True, how='left')
         self._df.drop(columns=['CCAA_altas'], inplace=True)
         # column deceased
         fac_df = pd.read_csv(self._source_path + '/ccaa_covid19_fallecidos_long.csv')
-        fac_df['fecha']= pd.to_datetime(fac_df['fecha'])
+        fac_df['fecha'] = pd.to_datetime(fac_df['fecha'])
         fac_df.set_index(['fecha', 'cod_ine'], inplace=True)
         fac_df.columns = ['CCAA_fac', 'deceased']
         self._df = self._df.merge(fac_df, left_index=True, right_index=True, how='left')
@@ -99,19 +98,19 @@ class COVID19Plot(object):
 
         self._global_df = pd.read_csv(csv_path)
         # convert date to datetime and set index
-        self._global_df['fecha']= pd.to_datetime(self._global_df['fecha'])
+        self._global_df['fecha'] = pd.to_datetime(self._global_df['fecha'])
         self._global_df.set_index(['fecha', 'cod_ine'], inplace=True)
         self._global_df.rename(columns={'total': 'cases'}, inplace=True)
         # column recovered
         altas_df = pd.read_csv(self._source_path + '/global_covid19_altas_long.csv')
-        altas_df['fecha']= pd.to_datetime(altas_df['fecha'])
+        altas_df['fecha'] = pd.to_datetime(altas_df['fecha'])
         altas_df.set_index(['fecha', 'cod_ine'], inplace=True)
         altas_df.columns = ['CCAA_altas', 'recovered']
         self._global_df = self._global_df.merge(altas_df, left_index=True, right_index=True, how='left')
         self._global_df.drop(columns=['CCAA_altas'], inplace=True)
         # column deceased
         fac_df = pd.read_csv(self._source_path + '/global_covid19_fallecidos_long.csv')
-        fac_df['fecha']= pd.to_datetime(fac_df['fecha'])
+        fac_df['fecha'] = pd.to_datetime(fac_df['fecha'])
         fac_df.set_index(['fecha', 'cod_ine'], inplace=True)
         fac_df.columns = ['CCAA_fac', 'deceased']
         self._global_df = self._global_df.merge(fac_df, left_index=True, right_index=True, how='left')
@@ -142,7 +141,7 @@ class COVID19Plot(object):
     def get_global_regions(self):
         return list(self._global_df.CCAA.unique())
 
-    def generate_plot(self, plot_type, region,language='en'):
+    def generate_plot(self, plot_type, region, language='en'):
         if plot_type not in self.PLOT_TYPES:
             raise RuntimeError('Plot type is not recognized')
 
@@ -179,7 +178,7 @@ class COVID19Plot(object):
         return image_fpath
 
     def _get_plot_data(self, plot_type, region):
-        df_reduced = self._df[self._df.CCAA==region]
+        df_reduced = self._df[self._df.CCAA == region]
         if plot_type == 'daily_cases':
             # calculate daily increase and rolling avg
             df_reduced['increase'] = df_reduced['cases'] - df_reduced['cases'].shift(1)
@@ -191,7 +190,7 @@ class COVID19Plot(object):
         return df_reduced
 
     def _get_global_plot_data(self, plot_type, region):
-        df_reduced = self._global_df[self._global_df.CCAA==region]
+        df_reduced = self._global_df[self._global_df.CCAA == region]
         if plot_type == 'daily_cases':
             # calculate daily increase and rolling avg
             df_reduced['increase'] = df_reduced['cases'] - df_reduced['cases'].shift(1)
@@ -218,14 +217,14 @@ class COVID19Plot(object):
             plt.bar(x, df['increase'], alpha=0.3, width=0.9, label=_('Daily increment'))
             plt.fill_between(x, 0, df['rolling'], color='red', alpha=0.5, label=_('Increment rolling avg (3 days)'))
             plt.plot(x, df['rolling'], color='red')
-            ax.annotate(f"{df['increase'][-1]:0,.0f}", xy=(x[-1]- np.timedelta64(12,'h'), df['increase'][-1]))
+            ax.annotate(f"{df['increase'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['increase'][-1]))
         elif plot_type == 'daily_deceased':
             title = _('Daily deaths evolution at {region}').format(region=_(region))
             y_label = _('Deaths')
             plt.bar(x, df['increase'], alpha=0.5, width=0.9, color='red', label=_('Daily deaths'))
             plt.fill_between(x, 0, df['rolling'], color='red', alpha=0.2, label=_('Deaths rolling avg (3 days)'))
             plt.plot(x, df['rolling'], color='red')
-            ax.annotate(f"{df['increase'][-1]:0,.0f}", xy=(x[-1]- np.timedelta64(12,'h'), df['increase'][-1]))
+            ax.annotate(f"{df['increase'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['increase'][-1]))
         elif plot_type == 'active_recovered_deceased':
             title = _('Active cases, recovered and deceased at {region}').format(region=_(region))
             y_label = _('Cases')
@@ -247,28 +246,28 @@ class COVID19Plot(object):
             title = _('Active cases at {region}').format(region=_(region))
             y_label = _('Cases')
             plt.bar(x, df['active_cases'], alpha=0.5, width=1, label=_('Active cases'))
-            ax.annotate(f"{df['active_cases'][-1]:0,.0f}", xy=(x[-1]  - np.timedelta64(12,'h'), df['active_cases'][-1]))
+            ax.annotate(f"{df['active_cases'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['active_cases'][-1]))
         elif plot_type == 'recovered':
             title = _('Recovered cases at {region}').format(region=_(region))
             y_label = _('Cases')
             plt.bar(x, df['recovered'], alpha=0.5, width=1, color='g', label=_('Recovered cases'))
-            ax.annotate(f"{df['recovered'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12,'h'), df['recovered'][-1]))
+            ax.annotate(f"{df['recovered'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['recovered'][-1]))
         elif plot_type == 'deceased':
             title = _('Deaths evolution at {region}').format(region=_(region))
             y_label = _('Deaths')
             plt.bar(x, df['deceased'], alpha=0.5, width=1, color='r', label=_('Deceased'))
-            ax.annotate(f"{df['deceased'][-1]:0,.0f}", xy=(x[-1]- np.timedelta64(12,'h'), df['deceased'][-1]))
+            ax.annotate(f"{df['deceased'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['deceased'][-1]))
         elif plot_type == 'cases_normalized':
             title = _('Cases per 100k inhabitants at {region}').format(region=_(region))
             y_label = _('Cases')
             plt.bar(x, df['cases_per_100k'], alpha=0.5, width=1, label=_('Active cases'))
-            ax.annotate(f"{df['cases_per_100k'][-1]:0,.0f}", xy=(x[-1]  - np.timedelta64(12,'h'), df['cases_per_100k'][-1]))
+            ax.annotate(f"{df['cases_per_100k'][-1]:0,.0f}", xy=(x[-1] - np.timedelta64(12, 'h'), df['cases_per_100k'][-1]))
 
         plt.title(title, fontsize=26)
-        ax.set_ylabel(y_label,fontsize=15)
+        ax.set_ylabel(y_label, fontsize=15)
         ax.set_xlim(np.datetime64('2020-03-01'))
         ax.figure.autofmt_xdate()
-        ax.legend(loc='upper left',fontsize=17)
+        ax.legend(loc='upper left', fontsize=17)
         if region in self.get_regions():
             self._add_footer(ax)
         elif region in self.get_global_regions():
@@ -290,43 +289,44 @@ class COVID19Plot(object):
             legend = False
             title = _('Cases per 100k inhabitants')
             y_label = _('Cases')
-            for region in regions:                
-                df_region = self._df[self._df.CCAA==region]
+            for region in regions:
+                df_region = self._df[self._df.CCAA == region]
                 x = df_region.index.get_level_values('fecha')
                 plt.plot(x, df_region['cases_per_100k'])
                 region_name = _(region)
-                ax.annotate(f"{df_region['cases_per_100k'][-1]:0,.0f} ({region_name})", xy=(x[-1]  - np.timedelta64(12,'h'), df_region['cases_per_100k'][-1]))
+                ax.annotate(f"{df_region['cases_per_100k'][-1]:0,.0f} ({region_name})", xy=(x[-1] - np.timedelta64(12, 'h'), df_region['cases_per_100k'][-1]))
         elif plot_type == 'deceased_normalized':
             legend = False
             title = _('Deceased per 100k inhabitants')
             y_label = _('Deaths')
-            for region in regions:                
-                df_region = self._df[self._df.CCAA==region]
+            for region in regions:
+                df_region = self._df[self._df.CCAA == region]
                 x = df_region.index.get_level_values('fecha')
                 df_region['deaths_per_100k'] = self._df['deceased'] * 100_000 / self._df['population']
                 plt.plot(x, df_region['deaths_per_100k'])
                 region_name = _(region)
-                ax.annotate(f"{df_region['deaths_per_100k'][-1]:0,.0f} ({region_name})", xy=(x[-1]  - np.timedelta64(12,'h'), df_region['deaths_per_100k'][-1]))
+                ax.annotate(f"{df_region['deaths_per_100k'][-1]:0,.0f} ({region_name})", xy=(x[-1] - np.timedelta64(12, 'h'), df_region['deaths_per_100k'][-1]))
 
-        
         plt.title(title, fontsize=26)
-        ax.set_ylabel(y_label,fontsize=15)
+        ax.set_ylabel(y_label, fontsize=15)
         ax.set_xlim(np.datetime64('2020-03-01'))
         ax.figure.autofmt_xdate()
         if legend:
-            ax.legend(loc='upper left',fontsize=17)
+            ax.legend(loc='upper left', fontsize=17)
         self._add_footer(ax)
         plt.savefig(image_path)
         plt.close()
 
     def _add_footer(self, ax):
-        ax.set_xlabel(_('Generated by COVID19gram (telegram bot)\n Data source from Datadista (see https://github.com/datadista/datasets/)'),
-              position=(1., 0.),
-              fontproperties=self._footer_font,
-              horizontalalignment='right')
+        ax.set_xlabel(
+            _('Generated by COVID19gram (telegram bot)\n Data source from Datadista (see https://github.com/datadista/datasets/)'),
+            position=(1., 0.),
+            fontproperties=self._footer_font,
+            horizontalalignment='right')
 
     def _add_global_footer(self, ax):
-        ax.set_xlabel(_('Generated by COVID19gram (telegram bot)\n Data source from covid19.health (see https://github.com/stevenliuyi/covid19/)'),
-              position=(1., 0.),
-              fontproperties=self._footer_font,
-              horizontalalignment='right')
+        ax.set_xlabel(
+            _('Generated by COVID19gram (telegram bot)\n Data source from covid19.health (see https://github.com/stevenliuyi/covid19/)'),
+            position=(1., 0.),
+            fontproperties=self._footer_font,
+            horizontalalignment='right')
