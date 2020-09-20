7af879635f58bcce1114cef1a36ad3384557cfd6
Jaume Perello <jaumeperello@gmail.com>
2020-09-20 19:35:59 +0200

    db.sql updated

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Apr 14, 2020 at 02:22 AM
--- Server version: 5.7.29-0ubuntu0.18.04.1
--- PHP Version: 7.1.17-0ubuntu0.17.10.1
+-- Host: localhost
+-- Generation Time: Sep 20, 2020 at 07:34 PM
+-- Server version: 5.7.29-0ubuntu0.16.04.1
+-- PHP Version: 7.2.29-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -36,23 +36,28 @@ CREATE TABLE `hashImage` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lang`
---
-
-CREATE TABLE `lang` (
-  `tg_id` bigint(20) NOT NULL,
-  `lang` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `subs`
 --
 
 CREATE TABLE `subs` (
   `user_id` bigint(20) NOT NULL,
   `region` text CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `tg_id` bigint(20) NOT NULL,
+  `lang` text NOT NULL,
+  `n_world` tinyint(1) NOT NULL DEFAULT '0',
+  `n_spain` tinyint(1) NOT NULL DEFAULT '0',
+  `n_italy` tinyint(1) NOT NULL DEFAULT '0',
+  `n_france` tinyint(1) NOT NULL DEFAULT '0',
+  `botons` set('gl','es','it','fr') NOT NULL DEFAULT 'gl,es,it'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -66,16 +71,16 @@ ALTER TABLE `hashImage`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lang`
---
-ALTER TABLE `lang`
-  ADD UNIQUE KEY `id` (`tg_id`);
-
---
 -- Indexes for table `subs`
 --
 ALTER TABLE `subs`
   ADD KEY `s_key` (`user_id`,`region`(255));
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`tg_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -85,7 +90,7 @@ ALTER TABLE `subs`
 -- AUTO_INCREMENT for table `hashImage`
 --
 ALTER TABLE `hashImage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24039;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
